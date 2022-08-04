a55205d24c7fab919e1efe4548fce23da433226a
hayoungzzzzz <jhy901@naver.com>
2022-08-04 18:17:17 +0900

    FEAT : 문제 생성 기능 구현중 - 문제 생성 시작 화면 ( 문제집 이름 설정 ) - 문제 생성 화면 ( 각 문제당 문제이름, 문제유형 (선택만 가능함),   답안입력(아직 텍스트만), 수정 . redux-persist에 저장하여 브라우저를   벗어나더라도 이어서 문제  출제 할 수 있게끔 함.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,8 @@ import { store, wrapper, persistor } from 'store';
 import { PersistGate } from 'redux-persist/integration/react';
 import axios from 'utils/customAxios';
 import { RootState } from 'store';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
+import { GetServerSidePropsCallback } from 'next-redux-wrapper';
 // NextPageWithLayout으로 Page의 타입을 지정하면,
 // getLayout 속성함수를 사용할 수 있게된다. (사용해도 되고 안해도 되고 )
 export type NextPageWithLayout = NextPage & {
@@ -22,15 +23,20 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // 페이지 단위에서 정의한 레이아웃이 있다면 해당 레이아웃을 적용한다.
   const { isLoggedin } = useSelector((state: RootState) => state.user);
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
 
   useEffect(() => {
-    if (isLoggedin) Router.push('/home');
+    if (isLoggedin) {
+      const now_router = router.pathname;
+      if (now_router === '/home') router.push('/home'); // 현재 라우터가 home 이라면 home으로
+      else router.push(now_router); // 현재 라우터가 home이 아니라면 기존에 있더 라우터로 
+    }
   }, [isLoggedin]);
-  
+
   return (
-    <Provider store={store}>
+    <>
       <PersistGate persistor={persistor}>{getLayout(<Component {...pageProps} />)}</PersistGate>
-    </Provider>
+    </>
   );
 }
 
@@ -38,9 +44,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 // _app 에서 getInitialProps 에서 가져오는 context의 타입은 AppContext임.
 // https://github.com/vercel/next.js/discussions/36832
 
-
 MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
-  
   const cookie = ctx.req ? ctx.req.headers.cookie : null;
   if (cookie) {
     // 빠르게 정규식으로 한다 .. 문자열 내장함수로 하게되면 매우 비효율적...
@@ -51,7 +55,9 @@ MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
     }
   }
   const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+
   return { pageProps };
 };
+
 
 export default wrapper.withRedux(MyApp);
