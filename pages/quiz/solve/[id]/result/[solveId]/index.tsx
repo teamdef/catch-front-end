411fa091b26538c44968a3820480b6f839c71f6d
jhwmac <jkr2505@naver.com>
2022-11-23 09:45:25 +0900

    FEAT: 결과화면 랭킹 제거, 리디자인, 한줄평 input 추가

--- pages/quiz/solve/[id]/result/[solveId]/index.tsx
+++ pages/quiz/solve/[id]/result/[solveId]/index.tsx
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 import { useState } from 'react';
 import type { ReactElement } from 'react';
+import { useInput } from 'hooks';
 import { AppLayout } from 'components/layout';
 import { useSelector } from 'react-redux';
 import Router, { useRouter } from 'next/router';
 import { RootState } from 'store';
-import { MainButton } from 'styles/common';
 import type { NextPageWithLayout } from 'pages/_app';
-import { HeadMeta, MatchNote, Logo, RankBoard, Button } from 'components/common';
-import ProgressBar from '@ramonak/react-progress-bar';
+import { HeadMeta, MatchNote, Logo, Button } from 'components/common';
+import { AiOutlineShareAlt } from 'react-icons/ai';
+// import ProgressBar from '@ramonak/react-progress-bar';
 
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
-  let { solveId } = router.query;
+  // let { solveId } = router.query;
+  const commCount = 232;
+  const [text, Setter, clearFunction, textHandler] = useInput<string>('');
   const { solveUserName, solveUserScore, solveProblems } = useSelector((state: RootState) => state.solve);
   const { isLoggedin } = useSelector((state: RootState) => state.user);
   const [openMatch, setOpenMatch] = useState<Boolean>(false);
@@ -24,40 +27,79 @@ const Page: NextPageWithLayout = () => {
         <h1>
           <strong>{solveUserName}</strong> 님
         </h1>
-        <ProgressArea>
+        {/* <ProgressArea>
           <ProgressBar completed={`${solveUserScore}`} maxCompleted={solveProblems.length} bgColor={'#ff4d57'} />
-        </ProgressArea>
+        </ProgressArea> */}
+        <UserScore>
+          <b>{(solveUserScore / solveProblems.length) * 100}</b>
+        </UserScore>
         <p>
           총 <span>{solveProblems.length}</span> 문제 중 <span>{solveUserScore}</span> 문제 맞았어요{' '}
         </p>
-        <Button
-          width="40%"
-          height="35px"
-          fontSize=".9rem"
-          bgColor="#fff"
-          fontColor="#ff4d57"
-          onClick={() => setOpenMatch(true)}
-        >
-          오답 노트
-        </Button>
       </ScoreArea>
-
-      <RankingArea>
+      <ButtonArea>
+        <Button onClick={() => Router.push(`${isLoggedin ? '/quiz/create' : '/'}`)}>나도 만들기</Button>
+        <Button onClick={() => setOpenMatch(true)}>오답 노트</Button>
+      </ButtonArea>
+      <ShareArea>
+        <h2>
+          <AiOutlineShareAlt />
+          퀴즈세트를 공유해보세요 !
+        </h2>
+        <div>
+          <ul>
+            <li>
+              <a href="">
+                <img src="/assets/img/instagram_icon.png"></img>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/assets/img/facebook_icon.png"></img>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/assets/img/kakao.png"></img>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="/assets/img/share_icon.png"></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </ShareArea>
+      <CommentArea>
+        <h2>
+          한줄평 남기기 <span style={{ fontSize: '.7rem' }}>({commCount})</span>
+        </h2>
+        <div>
+          <span>({text.length}/50)</span>
+          <input
+            type="text"
+            value={text}
+            onChange={textHandler}
+            id="comment-input"
+            maxLength={50}
+            placeholder="나도 한마디.."
+          />
+        </div>
+      </CommentArea>
+      {/* <RankingArea>
         <h2>
           <strong>{solveUserName}</strong> 님의 랭킹을 확인해 보세요!
         </h2>
-        <RankBoard solveId={solveId} />
-      </RankingArea>
+      </RankingArea> */}
 
-      <ButtonArea>
-        <MainButton onClick={() => Router.push(`${isLoggedin ? '/quiz/create' : '/'}`)}>나도 퀴즈 만들기</MainButton>
-      </ButtonArea>
       {openMatch ? <MatchNote setOpenMatch={setOpenMatch} /> : ''}
     </Container>
   );
 };
 const Container = styled.div`
   position: relative;
+  padding: 0 10%;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -77,8 +119,16 @@ const ButtonArea = styled.div`
   position: relative;
   display: flex;
   width: 100%;
-  justify-content: space-evenly;
+  gap: 3%;
+  justify-content: center;
   align-items: center;
+  button {
+    width: 40%;
+    height: 42px;
+    font-size: 0.9rem;
+    background-color: #ff4d57;
+    color: #fff;
+  }
 `;
 const ScoreArea = styled.div`
   position: relative;
@@ -109,120 +159,215 @@ const ScoreArea = styled.div`
     border: 2px solid #ff4d57;
   }
 `;
-const ProgressArea = styled.div`
+const UserScore = styled.div`
+  position: relative;
+  b {
+    position: relative;
+    font-size: 3.5rem;
+    color: #ff4d57;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      display: block;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 0;
+      width: 110%;
+      height: 4px;
+      border-radius: 2px;
+      background-color: #ff4d57;
+    }
+    &::after {
+      width: 130%;
+      bottom: -5px;
+    }
+  }
+`;
+const ShareArea = styled.div`
   position: relative;
   display: block;
   width: 80%;
-  span {
-    padding: 0 15px !important;
-    display: none !important;
-  }
-`;
-const RankingArea = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  > h2 {
-    margin-top: 0;
-    font-weight: normal;
-    color: #888;
-    font-size: 1rem;
-    strong {
-      font-weight: 500;
-      color: #ff4d57;
-    }
-  }
-  ul {
+  h2 {
     position: relative;
     display: flex;
-    flex-wrap: wrap;
-    padding: 0;
-    margin: 0;
-    gap: 4px;
-    flex-direction: column;
+    justify-content: center;
+    gap: 5px;
     align-items: center;
-    width: 80%;
-    li {
+    font-size: 0.8rem;
+    color: #888;
+    font-weight: 400;
+  }
+  > div {
+    position: relative;
+
+    ul {
       position: relative;
-      display: block;
-      color: #595959;
-      width: 100%;
-      list-style: none;
-      border-radius: 4px;
-      color: #595959;
-      font-size: 0.9rem;
-      border: 1px solid #f6f6f6;
-      > div {
+      display: flex;
+      justify-content: center;
+      padding: 0;
+      li {
         position: relative;
-        height: 44px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: #fff;
-      }
-      span {
-        position: relative;
-        width: 44px;
-        height: 44px;
-      }
-      strong {
-        font-weight: normal;
-      }
-      i,
-      em {
-        display: flex;
-        justify-content: center;
-        font-style: normal;
-        width: 50px;
-      }
-      i {
-        color: #ff4d57;
-        font-size: 1rem;
-      }
-      &.rank_1 {
-        border: 1px solid #fff1b4;
-        > div {
-          background-color: #fff1b4;
-        }
-      }
-      &.rank_2 {
-        border: 1px solid #ececec;
-        > div {
-          background-color: #ececec;
-        }
-      }
-      &.rank_3 {
-        border: 1px solid #ffe6d4;
-        > div {
-          background-color: #ffe6d4;
-        }
-      }
-      @keyframes AnimateBG {
-        0% {
-          background-position: 0% 50%;
-        }
-        100% {
-          background-position: -150% 50%;
-        }
-      }
-      &.active {
-        box-shadow: 0px 0px 10px 5px rgba(255, 165, 170, 0.5);
-        background-size: 300% 300%;
-        background-image: linear-gradient(-45deg, #ff4d57 0%, #ff4d57 10%, #fff 20%, #ff4d57 30%, #ff4d57 100%);
-        animation: AnimateBG 2s cubic-bezier(1, 0, 0.2, 0.2) infinite;
-        border: none;
-        > div {
-          margin: 3px;
-        }
-        strong {
-          font-weight: 500;
+        margin: 0 4%;
+        list-style: none;
+        a {
+          position: relative;
+          width: 38px;
+          height: 38px;
+          img {
+            width: 100%;
+            height: 100%;
+          }
         }
       }
     }
   }
 `;
+const CommentArea = styled.div`
+  position: relative;
+  width: 100%;
+  h2 {
+    font-size: 0.9rem;
+    color: #ff4d57;
+  }
+  div {
+    position:relative;
+    > span {
+    position: absolute;
+    font-size: .6rem;
+    color: #888;
+    bottom: 100%;
+    right: 0;
+    }
+    input {
+      background-color: #f4f4f4;
+      width: 100%;
+      height: 40px;
+      border: none;
+      padding: 0 5%;
+      color: #888;
+      border-radius: 4px;
+      ::placeholder {
+        color: #aaa;
+      }
+    }
+  }
+`;
+// const ProgressArea = styled.div`
+//   position: relative;
+//   display: block;
+//   width: 80%;
+//   span {
+//     padding: 0 15px !important;
+//     display: none !important;
+//   }
+// `;
+// const RankingArea = styled.div`
+//   position: relative;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   width: 100%;
+//   > h2 {
+//     margin-top: 0;
+//     font-weight: normal;
+//     color: #888;
+//     font-size: 1rem;
+//     strong {
+//       font-weight: 500;
+//       color: #ff4d57;
+//     }
+//   }
+//   ul {
+//     position: relative;
+//     display: flex;
+//     flex-wrap: wrap;
+//     padding: 0;
+//     margin: 0;
+//     gap: 4px;
+//     flex-direction: column;
+//     align-items: center;
+//     width: 80%;
+//     li {
+//       position: relative;
+//       display: block;
+//       color: #595959;
+//       width: 100%;
+//       list-style: none;
+//       border-radius: 4px;
+//       color: #595959;
+//       font-size: 0.9rem;
+//       border: 1px solid #f6f6f6;
+//       > div {
+//         position: relative;
+//         height: 44px;
+//         display: flex;
+//         justify-content: space-between;
+//         align-items: center;
+//         background-color: #fff;
+//       }
+//       span {
+//         position: relative;
+//         width: 44px;
+//         height: 44px;
+//       }
+//       strong {
+//         font-weight: normal;
+//       }
+//       i,
+//       em {
+//         display: flex;
+//         justify-content: center;
+//         font-style: normal;
+//         width: 50px;
+//       }
+//       i {
+//         color: #ff4d57;
+//         font-size: 1rem;
+//       }
+//       &.rank_1 {
+//         border: 1px solid #fff1b4;
+//         > div {
+//           background-color: #fff1b4;
+//         }
+//       }
+//       &.rank_2 {
+//         border: 1px solid #ececec;
+//         > div {
+//           background-color: #ececec;
+//         }
+//       }
+//       &.rank_3 {
+//         border: 1px solid #ffe6d4;
+//         > div {
+//           background-color: #ffe6d4;
+//         }
+//       }
+//       @keyframes AnimateBG {
+//         0% {
+//           background-position: 0% 50%;
+//         }
+//         100% {
+//           background-position: -150% 50%;
+//         }
+//       }
+//       &.active {
+//         box-shadow: 0px 0px 10px 5px rgba(255, 165, 170, 0.5);
+//         background-size: 300% 300%;
+//         background-image: linear-gradient(-45deg, #ff4d57 0%, #ff4d57 10%, #fff 20%, #ff4d57 30%, #ff4d57 100%);
+//         animation: AnimateBG 2s cubic-bezier(1, 0, 0.2, 0.2) infinite;
+//         border: none;
+//         > div {
+//           margin: 3px;
+//         }
+//         strong {
+//           font-weight: 500;
+//         }
+//       }
+//     }
+//   }
+// `;
+
 Page.getLayout = function getLayout(page: ReactElement) {
   return <AppLayout>{page}</AppLayout>;
 };
