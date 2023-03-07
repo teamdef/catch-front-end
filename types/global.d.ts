3fcd908fef079ced92902a65bc918a5995d76f06
jhwmac <jkr2505@naver.com>
2023-03-07 16:46:52 +0900

    REFACTOR: main / matchnote style 중복코드 제거

--- types/global.d.ts
+++ types/global.d.ts
@@ -36,6 +36,7 @@ interface SolveProblemTypes {
   id:string; // 퀴즈별 아이디
   is_img:boolean; // 이미지형 텍스트형 구분
   prob_title: string; // 퀴즈 제목
+  prob_image: string;
 }
 
 // SolveProblemSetTypes - 퀴즈세트(풀이용) 정보
