1624d26bfd25354d3c94b9019218a5adc3408a2a
hayoungzzzzz <jhy901@naver.com>
2023-05-01 22:55:13 +0900

    FIX: 사용하지 않는 폴더 제거

--- components/notification/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseModal } from './BaseModal';
-export { default as PortalWrapper } from './PortalWrapper';
-export { default as NickNameModal } from './NickNameModal';
-export { default as BottomUpModal } from './BottomUpModal';
