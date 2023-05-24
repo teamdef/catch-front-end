6e85df3325e8a699545da4c310655f804f9a4de3
hayoungzzzzz <jhy901@naver.com>
2023-05-24 17:40:19 +0900

    fix: 사용하지 않는 구문 제거

--- components/common/index.ts
+++ components/common/index.ts
@@ -9,7 +9,6 @@ export { default as Logo } from './Logo';
 export { default as HeadMeta } from './HeadMeta';
 
 export { default as QuizCard } from './QuizCard';
-export { default as QuizMiniCard } from '../legacy/QuizMiniCard';
 export { default as MyQuizCard } from './MyQuizCard';
 export { default as RecentQuiz } from './RecentQuiz';
 export { default as PopularQuiz } from '../popular/PopularQuiz';
