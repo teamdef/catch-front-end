f3175d520bd509a349e89554b3a3a3144556b3e1
jhwmac <jkr2505@naver.com>
2023-08-03 19:43:15 +0900

    style : 로그인 디자인 적용

--- pages/member/login.tsx
+++ pages/member/login.tsx
@@ -5,12 +5,12 @@ import Router from 'next/router';
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
 import { AppLayout } from 'components/layout'; /* 컴포넌트 */
-import * as S from 'styles/member/login.style'; /* 스타일 코드 */
+import styled from 'styled-components';
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res, params }: GetServerSidePropsContext) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }: GetServerSidePropsContext) => {
   const cookie = req ? req?.headers?.cookie : null;
   if (cookie) {
-    let match = cookie.match(new RegExp('(^| )' + 'access_token' + '=([^;]+)'));
+    const match = cookie.match(new RegExp('(^| )' + 'access_token' + '=([^;]+)'));
     // 쿠키가 적용되어 있다면 (로그인 상태라면)
     // 로그인 상태에서는 로그인 페이지에 접근할 수 없으니 돌려보내야 한다.
     if (!!match === true) {
@@ -22,37 +22,45 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res, params 
   return { props: {} };
 };
 const Page: NextPageWithLayout = () => {
-  const redirectURI =
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_DEV_KAKAO_REDIRECT_URI
-      : process.env.NEXT_PUBLIC_DEPLOY_KAKAO_REDIRECT_URI;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${redirectURI}&response_type=code`;
+  const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+  const API_KEY = process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const goKakaoLogin = () => {
     Router.push(KAKAO_AUTH_URL);
   };
 
   return (
-    <S.Wrapper>
-      <S.MainContainer>
-        <S.Title>
-          <span>캐</span>
-          <span>치</span>
-          <span>캐</span>
-          <span>치</span>
-        </S.Title>
-        <S.SubTitle>
-          로그인 <strong>1</strong>초컷 하고 <br />
-          <strong>나만의 퀴즈</strong>를 만들어보세요!
-        </S.SubTitle>
-        <S.KakaoLoginBtn onClick={goKakaoLogin}>
-          <img src={'/assets/img/kakao_icon.png'} />
-          <span>카카오로 시작하기</span>
-        </S.KakaoLoginBtn>
-      </S.MainContainer>
-    </S.Wrapper>
+    <Wrapper>
+      <Logo src="/assets/img/rebranding/login_logo.svg" />
+      <LoginButton onClick={goKakaoLogin} />
+      <Background src="/assets/img/rebranding/amusement_park.svg" />
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Logo = styled.img`
+  padding-left: 14px;
+`;
+const LoginButton = styled.button`
+  margin-top: 7.4vh;
+  width: 190px;
+  height: 45px;
+  background-repeat: no-repeat;
+  background-image: url('/assets/img/rebranding/kakao_login.svg');
+`;
+const Background = styled.img`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+`;
 Page.getLayout = function getLayout(page: ReactElement) {
   return <AppLayout>{page}</AppLayout>;
 };
