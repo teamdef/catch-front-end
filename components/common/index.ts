348359d7952bb67651257c5d54551a238c26bc8f
jhwmac <jkr2505@naver.com>
2023-08-24 19:28:55 +0900

    fix : export file 정리

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,19 +1,14 @@
-export { default as Header } from './Header';
-export { default as Loading } from './Loading';
-export { default as Title } from './Title';
-export { default as SideBar } from './SideBar';
-export { default as ThumbnailChange } from './ThumbnailChange';
-export { default as NotFound } from './NotFound';
-export { default as Logo } from './Logo';
-export { default as HeadMeta } from './HeadMeta';
-
-export { default as QuizCard } from './QuizCard';
-export { default as MyQuizCard } from './MyQuizCard';
-export { default as RecentQuiz } from './RecentQuiz';
-export { default as PopularQuiz } from '../popular/PopularQuiz';
-export { default as SkeletonQuizCard } from './SkeletonQuizCard';
-export { default as ProfileImage } from './ProfileImage';
-
-export { default as Icon } from './Icon';
-
 export { default as Emotion } from './Emotion';
+export { default as Floating } from './Floating';
+export { default as Header } from './Header';
+export { default as HeadMeta } from './HeadMeta';
+export { default as Icon } from './Icon';
+export { default as ImageSetter } from './ImageSetter';
+export { default as Loading } from './Loading';
+export { default as Logo } from './Logo';
+export { default as MyQuizCard } from './MyQuizCard';
+export { default as NotFound } from './NotFound';
+export { default as ProfileImage } from './ProfileImage';
+export { default as QuizCard } from './QuizCard';
+export { default as SkeletonQuizCard } from './SkeletonQuizCard';
+export { default as SkeletonMyQuizCard } from './SkeletonMyQuizCard';
